99f9df2f70051a0b96027017aac7724a0ef573b2
Keerthithev <keerthiganthevarasa@gmail.com>
2025-09-23 00:07:54 +0530

    security: remove hardcoded MongoDB URI; require MONGODB_URI env

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
 export async function connectToDatabase() {
-  const envUri = process.env.MONGODB_URI;
-  const fallbackUri = 'mongodb+srv://KeerthiDev:9AkQP1TaAYasb09H@keerthidev.stiw0.mongodb.net/tecezetask?retryWrites=true&w=majority';
-  const mongoUri = envUri && envUri.trim().length > 0 ? envUri : fallbackUri;
+  const mongoUri = process.env.MONGODB_URI;
+  if (!mongoUri) {
+    throw new Error('MONGODB_URI is not set. Define it in backend/.env');
+  }
 
   mongoose.set('strictQuery', false);
   await mongoose.connect(mongoUri, {
